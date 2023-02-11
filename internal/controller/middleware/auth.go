c542f07b318eceb53407222cede35b870661b9d8
msjai <103417345+msjai@users.noreply.github.com>
2023-02-11 13:59:56 +0300

    inplemented auth errors

--- internal/controller/middleware/auth.go
+++ internal/controller/middleware/auth.go
@@ -15,22 +15,25 @@ const (
 
 type KeyForUserID string
 
+var KeyUserID KeyForUserID = "reader"
+
 var (
-	KeyUserID             KeyForUserID = "reader"
-	ErrNotAuthentificated              = errors.New("user not authenticated")
+	ErrNotAuthentificated          = errors.New("user not authenticated")
+	ErrNotAuthEmptyHeader          = errors.New("user not authenticated; no authorization header")
+	ErrNotAuthBadWrongHeaderFormat = errors.New("user not authenticated; wrong authorization header format")
 )
 
 func IdentifyUser(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		aHeader := r.Header.Get(AuthorizationHeader)
 		if aHeader == "" {
-			http.Error(w, ErrNotAuthentificated.Error(), http.StatusUnauthorized)
+			http.Error(w, ErrNotAuthEmptyHeader.Error(), http.StatusUnauthorized)
 			return
 		}
 
 		headerParts := strings.Split(aHeader, " ")
 		if len(headerParts) != 2 || headerParts[0] != "Bearer" {
-			http.Error(w, ErrNotAuthentificated.Error(), http.StatusUnauthorized)
+			http.Error(w, ErrNotAuthBadWrongHeaderFormat.Error(), http.StatusUnauthorized)
 			return
 		}
 
